c01e1d7e335d16f3a5752b50865f730202b82afd
obinnanprogrammed <obinnanwakwue10@gmail.com>
2024-01-27 14:04:36 -0600

    imports

--- App.js
+++ App.js
@@ -1,5 +1,10 @@
+import * as React from 'react';
+import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Alert, Text, View, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Swipeable } from 'react-native-gesture-handler/Swipeable';
 
 export default function App() {
   return (
