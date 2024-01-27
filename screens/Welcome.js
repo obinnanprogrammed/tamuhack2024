1595652ef0f40063696ef92d1e1863420476db18
Sophia Phu <sophiatiffphu@gmail.com>
2024-01-27 16:05:06 -0600

    set up button navigation :)

--- screens/Welcome.js
+++ screens/Welcome.js
@@ -6,11 +6,14 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import PalmTree from '../components/PalmTree';
 import TwoLayerButton from '../components/Button';
+import { useNavigation } from '@react-navigation/native';
 
 export default function Welcome() {
+    const navigation = useNavigation(); 
     const handleButtonPress = () => {
         // Your button press logic goes here
         console.log('Button pressed!');
+        navigation.navigate('Login')
     };
     return (
         <View style={styles.container}>
