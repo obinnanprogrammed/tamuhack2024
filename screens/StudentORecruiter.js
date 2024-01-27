e7e7c801ee46a810513e1bc41c850ef1a1936241
Peter <49101240+peterphann@users.noreply.github.com>
2024-01-27 16:01:01 -0600

    Create initial page

--- screens/StudentORecruiter.js
+++ screens/StudentORecruiter.js
@@ -4,11 +4,27 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Alert, Text, View, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import PalmTree from '../components/PalmTree';  
 
 export default function StudentORecruiter() {
     return (
-        <View>
-            <Text>FIXME: create student/recruiter stuff</Text>
+        <View style={styles.component}>
+            <PalmTree imgSource={require('../assets/palm.png')} children={(
+                <View style={styles.centeredItems}>
+                    <Text style = {styles.welcomeText}>Welcome</Text>
+                </View>
+            )} />
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    component: {
+        backgroundColor: '#F0F6E8'
+    },
+
+    centeredItems:{
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+})
