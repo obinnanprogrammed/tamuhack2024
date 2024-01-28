9b3670d4b4c8432fa83d6a060c77cc5fbdf0f2ee
Sophia Phu <sophiatiffphu@gmail.com>
2024-01-27 23:38:07 -0600

    text and buttons for the actual matches part lmaooooo

--- screens/Homepage.js
+++ screens/Homepage.js
@@ -1,8 +1,9 @@
 import * as React from 'react';
-import { StyleSheet, View, Text, TouchableOpacity, Image } from 'react-native';
+import { StyleSheet, View, Text, TouchableOpacity, Image, Dimensions} from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 import PalmTree from '../components/PalmTree';
 import Carousel from 'react-native-new-snap-carousel';
+
 export default function Home() {
   return (
     <LinearGradient
@@ -35,28 +36,33 @@ export default function Home() {
                 <Carousel
                     data={[
                     {
-                        title: 'Item 1',
-                        text: 'Text 1',
+                        title: 'Joe Mama',
+                        text: 'Da googs',
+                        skillsText: 'Skills: React Native, Java, C++',
                         image: require('../assets/theHomieMark.jpg'),
                     },
                     {
-                        title: 'Item 2',
-                        text: 'Text 2',
+                        title: 'Jackson Blaziken',
+                        text: 'Jane Street ',
+                        skillsText: 'Skills: Getting GPA Gamed',
                         image: require('../assets/theHomieMark.jpg'),
                     },
                     {
-                        title: 'Item 3',
-                        text: 'Text 3',
+                        title: 'Dahomie Mark',
+                        text: 'Community College',
+                        skillsText: 'Skills: Live. Laugh. Love.',
                         image: require('../assets/theHomieMark.jpg'),
                     },
                     {
-                        title: 'Item 4',
-                        text: 'Text 4',
+                        title: 'Ur Mom',
+                        text: 'TAMU',
+                        skillsText: 'Skills: Getting finessed by the homie Mark',
                         image: require('../assets/theHomieMark.jpg'),
                     },
                     {
-                        title: 'Item 5',
-                        text: 'Text 5',
+                        title: 'Yo Mama',
+                        text: 'Harvard',
+                        skillsText: 'Skills: Bruh imagine',
                         image: require('../assets/theHomieMark.jpg'),
                     },
                     ]}
@@ -74,6 +80,16 @@ export default function Home() {
                       source={item.image}
                       style={{ flex: 1, width: '100%', resizeMode: 'cover', borderRadius: 20 }}
                     />
+                        <View style={styles.overlayTextContainer}>
+                          <Text style={styles.overlayTextTitle}>{item.title}</Text>
+                          <Text style={styles.overlayText}>{item.text}</Text>
+                          <Text style={styles.overlayText}>{item.skillsText}</Text>
+                          <TouchableOpacity style={styles.showMoreButton}>
+                            <Text style={styles.showMoreButtonText}>
+                                Show More
+                            </Text>
+                          </TouchableOpacity>
+                        </View>
 
                     </View>
                     )}
@@ -81,6 +97,20 @@ export default function Home() {
                     itemWidth={390}
                     layout={'tinder'}
                 />
+              <View style={styles.circleButtonContainer}>
+                <TouchableOpacity style={styles.circleButton}>
+                  <Image
+                    source={require('../assets/x_icon.png')} // Specify your image path
+                    style={styles.circleButtonImage}
+                  />
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.circleButton}>
+                <Image
+                    source={require('../assets/heart_icon.png')} // Specify your image path
+                    style={styles.circleButtonImage}
+                  />
+                </TouchableOpacity>
+              </View>
             </View>
           </View>
         )}
@@ -109,5 +139,50 @@ const styles = StyleSheet.create({
   settingsIcon: {
     width: 40,
     height: 40,
+  },
+  overlayTextContainer: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    padding: 20,
+    marginBottom: 40,
+  },
+  overlayTextTitle: {
+    fontSize: 40,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+  overlayText: {
+    fontSize: 16,
+    color: 'white',
+  },
+  showMoreButton: {
+    marginTop: 10,
+    padding: 5,
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: 'white',
+    width: 90,
+  },
+  showMoreButtonText: {
+    color: 'white',
+  },
+  circleButtonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingHorizontal: 80,
+  },
+  circleButton: {
+    width: 60,
+    height: 60,
+    borderRadius: 40,
+    backgroundColor: 'white',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginVertical: -30,
+  },
+  circleButtonImage: {
+    width: 50,
+    height: 50,
   },
 });
