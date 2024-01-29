ad42b0b19a16d2833d961eeddf9479ccec7b75e6
Peter <49101240+peterphann@users.noreply.github.com>
2024-01-29 13:14:39 -0600

    Add major selection

--- screens/UniAttend.js
+++ screens/UniAttend.js
@@ -1,5 +1,14 @@
 import * as React from "react";
-import { StyleSheet, Alert, Text, View, Button, TextInput, TouchableWithoutFeedback } from "react-native";
+import { useState, useEffect } from "react";
+import {
+  StyleSheet,
+  Alert,
+  Text,
+  View,
+  Button,
+  TextInput,
+  TouchableWithoutFeedback,
+} from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import PalmTree from "../components/PalmTree";
 import { LinearGradient } from "expo-linear-gradient";
@@ -9,6 +18,17 @@ import { Keyboard } from "react-native";
 
 export default function UniAttend() {
   const navigation = useNavigation();
+  const [university, setUniversity] = useState("");
+  const [major, setMajor] = useState("");
+
+  // Handle database stuff here
+  const handleUniversityInput = () => {
+    // Grab university and major here
+  };
+
+  useEffect(() => {
+    handleUniversityInput();
+  }, [university, major])
 
   return (
     <View style={styles.component}>
@@ -19,33 +39,36 @@ export default function UniAttend() {
           children={
             <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
               <View style={styles.wide}>
-              <BackNavBar
-                onPress={() => navigation.navigate("Student/Recruiter")}
-              ></BackNavBar>
-              <View style={styles.centeredItems}>
-                <Text style={styles.text}>What college do you attend?</Text>
-                <TextInput
-                  placeholder={"Type in your university"}
-                  style={[
-                    {
-                      borderColor: "black",
-                      borderBottomWidth: 1,
-                      paddingBottom: 10,
-                      marginBottom: 90,
-                      fontSize: 20,
-                      width: 300,
-                      textAlign: "center",
-                    },
-                  ]}
-                />
-                <View style={{ paddingTop: 40 }}>
-                  <BlueButton
-                    secondaryTitle="Next"
-                    onPress={() => navigation.navigate("Student Grad Date")}
-                  ></BlueButton>
+                <BackNavBar
+                  onPress={() => navigation.navigate("Student/Recruiter")}
+                ></BackNavBar>
+                <View style={styles.centeredItems}>
+                  <View style={styles.centeredItems}>
+                    <Text style={styles.text}>What college do you attend?</Text>
+                    <TextInput
+                      placeholder={"Enter your university"}
+                      style={styles.input}
+                      onChangeText={(text) => setUniversity(text)}
+                    />
+                  </View>
+
+                  <View style={styles.centeredItems}>
+                    <Text style={styles.majorText}>What is your major?</Text>
+                    <TextInput
+                      placeholder={"Enter your major"}
+                      style={styles.input}
+                      onChangeText={(text) => setMajor(text)}
+                    />
+                  </View>
+
+                  <View style={{ paddingTop: 40 }}>
+                    <BlueButton
+                      secondaryTitle="Next"
+                      onPress={() => navigation.navigate("Student Grad Date")}
+                    ></BlueButton>
+                  </View>
                 </View>
               </View>
-            </View>
             </TouchableWithoutFeedback>
           }
         />
@@ -82,5 +105,23 @@ const styles = StyleSheet.create({
   wide: {
     padding: 20,
     width: "100%",
+  },
+
+  majorText: {
+    color: "#000",
+    fontSize: 24,
+    fontWeight: "bold",
+    textAlign: "center",
+    marginBottom: 30,
+  },
+
+  input: {
+    borderColor: "black",
+    borderBottomWidth: 1,
+    paddingBottom: 10,
+    marginBottom: 90,
+    fontSize: 20,
+    width: 300,
+    textAlign: "center",
   },
 });
